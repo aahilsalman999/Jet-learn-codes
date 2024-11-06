7624ba0bcf51fc4781b56a03e9d504ac66505738
aahilsalman999 <aahilsalman1@hotmail.com>
2024-11-06 16:50:16 +0000

    Add files via upload

--- textbooks.py
+++ textbooks.py
@@ -1,9 +1,13 @@
 books = {"Physics" : "175" , "Chemistry" : "210" , "Biology" : "195" , "Maths" : "125" , "English" : "125"} 
 books["Physics"] = "200"
-num1extra_book = input("Select one more book")
-num2extra_book = input("Select another book")
-for i in books:
- books.append(i)
-books[num1extra_book] = "130"
-books[num2extra_book] = "130"
 print(books)
+books["History"] = "130"
+books ["Geography"] = "130"
+print(books)
+user_book = input("Select a book")
+if user_book in books:
+ print("The cost of",user_book, "is",books[user_book])
+else:
+ print("Book not found")
+for book,cost in books.items():
+ print(book,cost)
